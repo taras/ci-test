eafc700f1cf05eb0f81eaed1b631d6a7c5cb086c
Taras Mankovski <tarasm@gmail.com>
2014-01-29 11:34:29 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    "github-release": {
+      options: {
+        repository: 'dolbyzerr/grunt-github-releaser', // Path to repository
+        auth: {   // Auth credentials
+          user: 'dolbyzerr',
+          password: ''
+        }
+      },
+      files: {
+        src: ['dist/release.zip'] // Files that you want to attach to Release
+      }
+
+    }
+  });
+};
