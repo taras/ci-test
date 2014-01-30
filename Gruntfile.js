f61ae90fddb28efad0101ef8ac033917a9dc34c0
Taras Mankovski <tarasm@gmail.com>
2014-01-29 19:13:20 -0500

    Committed build for hello

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
         }
       },
       "push-build": {
-        command: "git push origin gh-pages",
+        command: "git push origin gh-pages/gh-pages",
         options: {
           cmd: 'gh-pages'
         }
