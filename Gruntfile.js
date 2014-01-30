460858f662b3849e18c99a255d2ad6a56cbbc4bb
Taras Mankovski <tarasm@gmail.com>
2014-01-29 19:07:22 -0500

    Committed build for hello

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,58 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
-    "github-release": {
-      options: {
-        repository: 'dolbyzerr/grunt-github-releaser', // Path to repository
-        auth: {   // Auth credentials
-          user: 'dolbyzerr',
-          password: ''
+    env: {
+      user: 'taras',
+      repository: 'ci-test',
+      commit: process.env.TRAVIS_COMMIT,
+      gh_token: process.env.GH_TOKEN
+    },
+    clean: {
+      "gh-pages": ['gh-pages']
+    },
+    zip: {
+      dist: {
+        cwd: 'src',
+        src: ['src/**/*'],
+        dest: 'gh-pages/builds/<%= env.commit %>.zip'
+      }
+    },
+    shell: {
+      "clone-gh-pages": {
+        command: "git clone https://<%= env.gh_token %>@github.com/<%= env.user %>/<%= env.repository %>.git --branch=gh-pages",
+        options: {
+          cwd: 'gh-pages'
         }
       },
-      files: {
-        src: ['dist/release.zip'] // Files that you want to attach to Release
+      "add-build": {
+        command: "git add .",
+        options: {
+          cmd: 'gh-pages'
+        }
+      },
+      "commit-build": {
+        command: "git commit -m 'Committed build for <%= env.commit %>'",
+        options: {
+          cmd: 'gh-pages'
+        }
+      },
+      "push-build": {
+        command: "git push origin gh-pages",
+        options: {
+          cmd: 'gh-pages'
+        }
       }
     }
   });
 
-  grunt.registerTask('test', function(){
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-zip');
 
-  });
+  grunt.registerTask('test', function(){});
+
+  grunt.registerTask('release', ['zip:dist'])
 
   grunt.registerTask('default', ['test']);
+
 };
